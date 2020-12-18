d2f8c451f07da9f31ce62ad4016726824857efe6
fredie <contact@funabia.com>
2020-12-18 21:47:21 +0000

    Add Divider component to root export and amend Card styling

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 export * from "./src/components/Button";
 export * from "./src/components/Card";
+export * from "./src/components/Divider";
 export * from "./src/components/FormGroup";
 export * from "./src/components/Input";
