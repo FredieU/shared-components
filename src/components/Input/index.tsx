6f10c0baa369c08f74981a4299ca50e0747d66d8
fredie <contact@funabia.com>
2020-12-18 03:45:03 +0000

    Add ref support to Input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,31 +1,41 @@
-import React, { ComponentPropsWithRef, ReactElement } from "react";
+import React, {
+  ComponentPropsWithRef,
+  forwardRef,
+  ReactElement,
+  Ref,
+} from "react";
 import classnames from "classnames";
 import "./Input.css";
 
 export type InputProps = ComponentPropsWithRef<"input"> & {
   className?: string;
-  hasErrors: boolean;
+  hasErrors?: boolean;
   label: string;
   name: string;
 };
 
-export function Input({
-  className,
-  hasErrors,
-  label,
-  name,
-  ...props
-}: InputProps): ReactElement {
-  const inputClasses = classnames("input", {
-    "input--errored": hasErrors,
-  });
+export const Input = forwardRef(
+  (
+    { className, hasErrors = false, label, name, ...rest }: InputProps,
+    ref?: Ref<HTMLInputElement>
+  ): ReactElement => {
+    const inputClasses = classnames("input", {
+      "input--errored": hasErrors,
+    });
 
-  return (
-    <>
-      <label className="input__label" htmlFor={name}>
-        {label}
-      </label>
-      <input id={name} className={inputClasses} {...props} />
-    </>
-  );
-}
+    return (
+      <>
+        <label className="input__label" htmlFor={name}>
+          {label}
+        </label>
+        <input
+          className={inputClasses}
+          name={name}
+          id={name}
+          ref={ref}
+          {...rest}
+        />
+      </>
+    );
+  }
+);
